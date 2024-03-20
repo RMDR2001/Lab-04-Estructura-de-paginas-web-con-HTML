96b9829c6b79d9843326b60ddeca8fa1d1c76d21
ricardo-mauricio-delgado-rivera <rmdr2001@gmail.com>
2024-03-19 21:26:39 -0500

    Correción en funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -9,9 +9,9 @@ let validar;
 
 // Funciones
 
-function comenzar(){
-    document.write("Bienvenido a mi WebPage hecho de mis clases de Code 102 de Enter Tech School!!!")
-}
+// function comenzar(){
+//     document.write("Bienvenido a mi WebPage hecho de mis clases de Code 102 de Enter Tech School!!!")
+// }
 
 function pedirNombre(){
     return prompt("Ingrese su nombre...");
