b61bf962810d974e80d3c9bffa6ce0191c17b4a5
ricardo-mauricio-delgado-rivera <rmdr2001@gmail.com>
2024-03-20 21:35:41 -0500

    Comentando opLogicos.js

--- js/opLogicos.js
+++ js/opLogicos.js
@@ -9,7 +9,12 @@ function calificar(){
         
     let contador = 0;
 
+    // Agrega validación en uno de los inputs del usuario
+
     while (ingreso === false) {
+
+        // Solicita al usuario una pregunta relevante para un número
+
         estrellas = prompt("Del 1 al 5, ¿Con cuántas estrellas calificarías la página?");
         if (estrellas === "" || estrellas === null){
             ingreso = false;
@@ -20,11 +25,16 @@ function calificar(){
     
     let numEstrellas = parseInt(estrellas);
     
-
+    // Utiliza un bucle en tu código JavaScript para mostrar la imagen tantas veces.
+    
     for (let i = 1 ; i<=numEstrellas ; i++) {
         calificacion = calificacion+"⭐";
 
-        // "Extender los objetivos" 
+        /********************************************************************************************************
+        "Extender los objetivos" 
+        A medida que visualizes cada imagen, muestra un contador al lado que indica el número de la imagen.
+        *********************************************************************************************************/
+
         contador++;
     }
     document.write("Gracias por la calificación<br>Su calificación: "+calificacion+"<br>"+contador+" estrella(s)")
