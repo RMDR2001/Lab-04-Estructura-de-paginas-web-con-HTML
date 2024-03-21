c7dc263464be6ff146099745d9493e08686e9794
ricardo-mauricio-delgado-rivera <rmdr2001@gmail.com>
2024-03-20 22:14:26 -0500

    Corrección mostrar calificacion

--- js/opLogicos.js
+++ js/opLogicos.js
@@ -18,7 +18,7 @@ function calificar(){
     // Utiliza un bucle en tu código JavaScript para mostrar la imagen tantas veces.
 
     for (let i = 1 ; i<=numEstrellas ; i++) {
-        calificacion = calificacion+"<img src='https://img.freepik.com/vector-gratis/estrella-metal-3d-aislada_1308-115283.jpg' width='50'>"+i;
+        calificacion = "<img src='https://img.freepik.com/vector-gratis/estrella-metal-3d-aislada_1308-115283.jpg' width='50'>"+i;
 
         /********************************************************************************************************
         "Extender los objetivos" 
